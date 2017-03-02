9c294e821118e5e656326f5953bed9f81170342c
Syed Saad <hassan.saad.mail@gmail.com>
2017-03-03 03:26:26 +0530

    Updates

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -172,6 +172,7 @@ public class SunshineSyncTask {
                                     }
                                 }
                             });
+                    cursor.close();
                 }
             }
 
